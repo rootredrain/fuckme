d81a56d98ccf38b928dfd4365beccb9e3ae5b3cb
rootredrain <rootredrain@gmail.com>
2015-09-02 20:46:43 +0800

    lib

--- lib/request.py
+++ lib/request.py
@@ -3,9 +3,14 @@
 #
 # do HTTP request
 #
-
+import copy    # deepcopy
 import requests
 from requests.auth import HTTPBasicAuth
+
+def fake_ip(self, local_headers):
+    if self.args.fip:    # Random IP
+        local_headers['X-Forwarded-For'] = local_headers['Client-IP'] = \
+            '.'.join(str(random.randint(1,255)) for _ in range(4))
 
 
 def do_request(self):
@@ -16,3 +21,13 @@ def do_request(self):
             thread_exit(self)
             return
 
+        if params is None:
+            self.queue.task_done()
+            thread_exit(self)
+            return
+
+        local_headers = copy.deepcopy(self.http_headers)
+        fake_ip(self, local_headers)
+
+        # data = self.args.query
+        
